3f9ed233bcf71ed1cc4903315658baf82a74005a
cmoreiraa <cmoreiraalen@danielcastelao.org>
2023-10-25 10:28:00 +0200

    Seguimos trabajando en la rama Compose

--- app/src/main/java/com/example/myapplication/IU.kt
+++ app/src/main/java/com/example/myapplication/IU.kt
@@ -6,29 +6,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.MyViewModel
-import com.example.myapplication.R
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 /**
@@ -38,7 +43,9 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 @Composable
 fun IU(miViewModel: MyViewModel) {
 
-    Box {
+    Box (
+        modifier = Modifier.fillMaxSize()
+    ){
         Image(
             painter = painterResource(R.drawable.balaidos),
             contentDescription = "icono",
@@ -46,14 +53,10 @@ fun IU(miViewModel: MyViewModel) {
                 .size(210.dp)
                 .offset(y = 120.dp)
                 .offset(x = 70.dp)
-            /* Icon(
-             painter = painterResource(id = R.drawable.tick),
-             contentDescription = "tick"
-            )*/
+
         )
         Text( //Línea de texto donde aparece la lista de numero aleatorios después de pulsar el botón
 
-            //text = "Numeros: ${miViewModel.getNumero()}",// Llama al método que me devuelve un número aleatorio
             text = "Numeros: ${miViewModel.getListaRandom()}",// Llama al método que me devuelve la lista de números aleatorios
             fontWeight = FontWeight.Light,
             modifier = Modifier
@@ -65,7 +68,6 @@ fun IU(miViewModel: MyViewModel) {
         Button( //Botón para generar la lista de números aleatorios
             onClick = { miViewModel.crearRandom() },
             modifier = Modifier
-                //.padding(vertical = 300.dp, horizontal = 90.dp)
                 .offset(y = 330.dp)
                 .offset(x = 40.dp)
 
@@ -82,11 +84,127 @@ fun IU(miViewModel: MyViewModel) {
             Text(text = "Click para generar números")
         }
 
-
     }
     Login(miViewModel)
+    juegoSimon(miViewModel)
 
 }
+
+@Composable
+fun juegoSimon(miViewModel: MyViewModel) {
+
+    Box{
+        // Cuadro de suma de rondas
+        if(miViewModel.getContador()<10){
+            Text("Ronda: ${miViewModel.getContador()}",
+                modifier = Modifier.offset(y=400.dp, x= 250.dp))
+        }else{
+            Text("Ronda: ${miViewModel.getContador()}",
+                modifier = Modifier.offset(y=400.dp, x= 250.dp), fontSize = 24.sp)
+        }
+
+
+        Button( //Botón azul
+            onClick = {},
+            modifier = Modifier
+                .offset(y = 470.dp)
+                .offset(x = 100.dp)
+                .size(40.dp)
+                .background(color = Color.Blue)
+
+
+        ){
+
+        }
+
+        Button( //Botón verde
+            onClick = {},
+            modifier = Modifier
+                .offset(y = 470.dp)
+                .offset(x = 200.dp)
+                .size(40.dp)
+                .background(color = Color.Green)
+
+
+
+        ){
+
+        }
+        Button( //Botón rojo
+            onClick = {},
+            modifier = Modifier
+                .offset(y = 530.dp)
+                .offset(x = 100.dp)
+                .size(40.dp)
+                .background(color = Color.Red)
+
+
+        ){
+
+        }
+
+        Button( //Botón amarillo
+            onClick = {},
+            modifier = Modifier
+                .offset(y = 530.dp)
+                .offset(x = 200.dp)
+                .size(40.dp)
+                .background(color = Color.Yellow)
+
+
+
+        ){
+
+        }
+
+
+
+        Button( //Botón PLAY
+            onClick = {miViewModel.contador()},
+            modifier = Modifier
+                .size(80.dp, 40.dp)
+                .offset(y = 610.dp)
+                .offset(x = 200.dp)
+
+
+
+
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.escudocelta),
+                contentDescription = "Play",
+                modifier = Modifier
+                    .size(100.dp)
+
+            )
+
+        }
+        startReset()
+    }
+}
+
+@Composable
+fun startReset() {
+    var isStart by remember { mutableStateOf(true) }
+
+    Button(
+        onClick = {
+            isStart = !isStart
+        },
+        modifier = Modifier
+            .size(80.dp, 45.dp)
+            .offset(y = 610.dp)
+            .offset(x = 70.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        Text(
+            text = if (isStart) "START" else "RESET", // Cambia el texto del botón
+            style = TextStyle (fontSize = 10.sp)
+        )
+    }
+
+}
+
 
 /**
  * Función del contador de clicks y de escribir texto en la caja
@@ -104,11 +222,11 @@ fun Login(miViewModel: MyViewModel) {
 
         // Caja de texto para escribir
         OutlinedTextField(
-            value = miViewModel.getString(), // Coge el valor de lo que escribo en de la caja de texto
-            onValueChange = {// Lo actualiza y muestra a medida que voy escribiendo
+            value = miViewModel.getString(),
+            onValueChange = {
                 miViewModel.name.value = it
             },
-            label = { Text(text = "Escribe:") } // Palabra que aparece dentro de la caja y que al escribir se pone como titulo de la misma.
+            label = { Text(text = "Escribe:") }
         )
 
         // Condición para que solo me muestre el contenido de la caja si escribo más de 3 caracteres
@@ -122,7 +240,7 @@ fun Login(miViewModel: MyViewModel) {
     }
 }
 
-
+/* ESTO NO LO USO
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     // var msj_saludo = stringResource(id = R.string.saludo)
@@ -134,14 +252,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     ) {
         Box {
             Image(
-                painter = painterResource(R.drawable.balaidos),
+                painter = painterResource(R.drawable.naturaleza),
                 contentDescription = "icono",
                 modifier = Modifier
                     .size(200.dp)
-                /* Icon(
-                     painter = painterResource(id = R.drawable.tick),
-                     contentDescription = "tick"
-                 )*/
             )
         }
         Column {
@@ -167,7 +281,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 
 }
-
+*/
 
 /**
  * Vista previa de la app
@@ -180,7 +294,7 @@ fun GreetingPreview() {
     }
 }
 
-
+/*
 //ESTO NO LO USAMOS, es un ejemplo
 @Composable
 fun InterfazUsuario() {
@@ -204,3 +318,4 @@ fun login() {
     texto_descriptivo(texto = "Fallo de login")
 }
 
+*/
